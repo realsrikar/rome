c5c5c22aed8804efe2a2b44d41ffdfbcaa10aef2
Srikar <30674879+realsrikar@users.noreply.github.com>
2017-12-01 21:14:48 -0500

    SASS is now in the project. Added readme and gulpfile.js

---
+++ README.md
@@ -0,0 +1,14 @@
+## How to set up the project
+
+
+### CLI
+
+1. You need to have npm installed. To get it, install [NodeJS](https://nodejs.org/en/)
+2. You need to have gulp installed globally. Type in `npm install -g gulp` to get it. (NOTE: you need npm before running this)
+3. Run `npm install` this will create the huge node_modules folder.
+4. Run `gulp`
+
+
+
+### GUI
+Use something like PrePros or CodeKit
---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const prefix = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+
+gulp.task('sass', function() {
+  gulp.src('sass/*')
+  .pipe(sass())
+  .pipe(prefix())
+  .pipe(gulp.dest('css/'))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('watch', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    gulp.watch("sass/*", ['sass']);
+    gulp.watch("*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['sass', 'watch'])
