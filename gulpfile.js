635dc60e7776f9b0445924dc21c105fe3a4df961
Srikar <30674879+realsrikar@users.noreply.github.com>
2017-12-07 17:46:34 -0500

    Changes for BrowserSyncin gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,29 @@
-const gulp        = require('gulp');
-const sass        = require('gulp-sass');
-const prefix      = require('gulp-autoprefixer');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const prefix = require('gulp-autoprefixer');
 const browserSync = require('browser-sync').create();
 
 gulp.task('sass', function() {
   gulp.src('sass/*')
-  .pipe(sass().on('error', sass.logError))
-  .pipe(prefix())
-  .pipe(gulp.dest('css/'))
-  .pipe(browserSync.stream());
+    .pipe(sass().on('error', sass.logError))
+    .pipe(prefix())
+    .pipe(gulp.dest('css/'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('watch', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        },
-    });
+gulp.task('serve', function() {
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    },
+    ui: false,
+    port: 7800,
+    browser: "Google Chrome",
+    notify: false,
+  });
 
-    gulp.watch("sass/*", ['sass']);
-    gulp.watch("*.html").on('change', browserSync.reload);
+  gulp.watch("sass/*", ['sass']);
+  gulp.watch("*.html").on('change', browserSync.reload);
 });
 
-gulp.task('default', ['sass', 'watch'])
+gulp.task('default', ['sass', 'serve'])
