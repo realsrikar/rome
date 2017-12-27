b53d0fa07f28baf8bd9cfb01debf964d49d9fb1a
rod <rod_rahmjoo@yahoo.com>
2017-12-26 22:37:04 -0500

    not working php

---
+++ email.php
@@ -0,0 +1,25 @@
+<?php
+
+// This two steps to help avoid spam; found it online hope it works
+$headers .= "Message-ID: <".time()." TheSystem@".$_SERVER['SERVER_NAME'].">\r\n";
+$headers .= "X-Mailer: PHP v".phpversion()."\r\n";
+
+
+$name=$_Post['name'];
+$email=$_Post['email'];
+$comments=$_Post['comments'];
+
+$mailto="YOUREMAIL@somethingsomewhere.com";//enter your email here to test
+$headers="From:" .$email
+$txt="You have recieved an email from " .$name;
+$subject="New Message"
+
+$emailform=mail ($to, $subject, $comments, "From: " . $name .);
+
+if($emailform){
+echo "Thank You For Your Input";
+}
+else {
+echo "ERROR";
+}
+?>
