d9a583ce7f5e2a4f78312ad992871a725d0a3113
Srikar <ssiyer0208@gmail.com>
2018-01-15 20:12:15 -0500

    bug fixed to email php

--- email.php
+++ email.php
@@ -1,19 +1,20 @@
 <?php
 
 // This two steps to help avoid spam; found it online hope it works
-$headers .= "Message-ID: <".time()." TheSystem@".$_SERVER['SERVER_NAME'].">\r\n";
+$headers = "Message-ID: <".time()." TheSystem@".$_SERVER['SERVER_NAME'].">\r\n";
 $headers .= "X-Mailer: PHP v".phpversion()."\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
 
 $name=$_Post['name'];
 $email=$_Post['email'];
 $comments=$_Post['comments'];
 $to="romanthemes@gmail.com";
-$headers="From:" .$email;
+$headers.="From: " .$email;
 $txt="You have recieved an email from " .$name;
 $subject="New Message";
 
-$emailform=mail($to, $subject, $comments, "From: " .$name);
+$emailform=mail($to, $subject, $comments, $headers);
 
 if($emailform){
 echo "Thank You For Your Input";
