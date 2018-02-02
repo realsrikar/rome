bd91bfa9a2dba967983a8ee4771c30edee1969bd
Srikar <ssiyer0208@gmail.com>
2018-02-01 21:10:44 -0500

    add comment

--- email.php
+++ email.php
@@ -15,6 +15,7 @@ $txt="You have recieved an email from " .$name;
 $subject="New Message";
 
 $emailform = @mail($to, $subject, $comments, $headers);
+// added the "@" before "mail"
 
 if($emailform){
 echo "Thank You For Your Input";
